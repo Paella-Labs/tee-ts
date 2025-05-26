f0f40b00958efe6ee2719749f9f85a78b81ecb61
Alberto García Planes <alberto@paella.dev>
2025-05-26 21:32:52 +0200

    better

--- src/services/key.service.ts
+++ src/services/key.service.ts
@@ -5,7 +5,10 @@ import { secp256k1 } from "ethereum-cryptography/secp256k1.js";
 import { sha256 } from "ethereum-cryptography/sha256.js";
 import { toHex } from "ethereum-cryptography/utils";
 import type { PublicKeyResponse } from "types";
-const SECP256K1_DERIVATION_PATH = new Uint8Array([0x42]);
+const SECP256K1_DERIVATION_PATH = new Uint8Array([
+	0x73, 0x65, 0x63, 0x70, 0x32, 0x35, 0x36, 0x6b, 0x31, 0x2d, 0x64, 0x65, 0x72,
+	0x69, 0x76, 0x61, 0x74, 0x69, 0x6f, 0x6e, 0x2d, 0x70, 0x61, 0x74, 0x68,
+]);
 
 /**
  * Service for key derivation and management
